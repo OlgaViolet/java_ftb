44014f4186d417a9da25d22928b958804c18ee55
OlgaViolet <60447601+OlgaViolet@users.noreply.github.com>
2020-05-21 17:19:34 -0400

    Added few functions (methods)

--- sandbox/src/main/java/ru/stqa/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/sandbox/MyFirstProgram.java
@@ -2,7 +2,29 @@ package ru.stqa.sandbox;
 
 public class MyFirstProgram {
     public static void main(String[] args) {
+        hello("World");
+        hello("user");
+        hello("Olga");
 
-        System.out.println("Hello, World!");
+        double len = 7;
+        System.out.println("Area of the square with the side of " + len + " = " + area(len));
+
+        double length = 3;
+        double width = 5;
+        System.out.println("Area of a parallelogram with sides of " + width + " and " + length + " = " + area(length, width));
+    }
+    public static void hello(String somebody) {
+        System.out.println("Hello, " + somebody + "!");
+
+    }
+
+    public static double area (double l) {
+       return l * l;
+    }
+
+
+    public static double area (double length, double width) {
+
+        return length * width;
     }
 }
