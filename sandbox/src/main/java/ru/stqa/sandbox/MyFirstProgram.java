92fc4e938ee21fe4d603fb5ac271586f7674d529
OlgaViolet <60447601+OlgaViolet@users.noreply.github.com>
2020-05-21 15:44:25 -0400

    Indent updated

--- sandbox/src/main/java/ru/stqa/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/sandbox/MyFirstProgram.java
@@ -1,8 +1,8 @@
 package ru.stqa.sandbox;
 
 public class MyFirstProgram {
-	public static void main (String [] args) {
+    public static void main(String[] args) {
 
-		System.out.println("Hello, World!");
-	}
+        System.out.println("Hello, World!");
+    }
 }
